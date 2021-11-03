367bb4945793762454b17936b495b2fb6efe81f8
Temba Mateke <tembamateke@gmail.com>
2021-11-03 15:54:01 -0400

    Add HelloWorld Schema

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    graphiql: true,
-  })
-);
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "helo world",
+      },
+    }),
+  }),
+});
+
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 app.listen(5000, () => {
   console.log("Server Running");
 });

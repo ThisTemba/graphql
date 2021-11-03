4ae81f049f26f49ce55ceb6eb41eeef1df380dec
Temba Mateke <tembamateke@gmail.com>
2021-11-03 16:17:40 -0400

    Add books schema

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
+const { query } = require("express");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
 const app = express();
 const { books, authors } = require("./data.js");
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "HelloWorld",
-    fields: () => ({
-      message: {
-        type: GraphQLString,
-        resolve: () => "helo world",
-      },
-    }),
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "this is a book",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
   }),
 });
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "a list of books",
+      resolve: () => books,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({ query: RootQueryType });
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 app.listen(5000, () => {

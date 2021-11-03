180918563874f97c480f6dfed867aa4a77f749ba
Temba Mateke <tembamateke@gmail.com>
2021-11-03 15:46:22 -0400

    Add expressgraphql whatever that is

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
 app.listen(5000, () => {
   console.log("Server Running");
 });

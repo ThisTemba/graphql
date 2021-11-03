999397bb90918db9f65d7c1a7c9e4149d831e7ed
Temba Mateke <tembamateke@gmail.com>
2021-11-03 16:01:38 -0400

    Add books & authors data

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
+const { books, authors } = require("./data.js");
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
